1468ae101da48e3c787e015d57788c8a2692e446
Nguyen Duc Dung <nguyenducdung15082002@gmail.com>
2022-09-06 19:10:20 +0700

    Button on Main Activity help switch to the Question Activity

--- app/src/main/java/com/example/quiz_app/MainActivity.kt
+++ app/src/main/java/com/example/quiz_app/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.quiz_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttonSwitchScreen : Button = findViewById(R.id.button_switch_screen)
+        val editTextName : EditText = findViewById(R.id.edit_text_name)
+
+        buttonSwitchScreen.setOnClickListener{
+            if(editTextName.text.isEmpty()){
+                Toast.makeText(this, "Please Enter Your name before submit", Toast.LENGTH_LONG).show()
+            }
+            else{
+                val intent = Intent(this, QuestionActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/quiz_app/QuestionActivity.kt
+++ app/src/main/java/com/example/quiz_app/QuestionActivity.kt
@@ -2,10 +2,12 @@ package com.example.quiz_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class QuestionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question)
+
     }
 }
